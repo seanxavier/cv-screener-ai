a8a3a3b0239bf1066af5f0162173bcffb49e0541
Sean Xavier Nieva <sean.xaviernieva@gmail.com>
2025-03-04 14:13:24 +0800

    frontend chat

--- README.md
+++ README.md
@@ -15,3 +15,16 @@ powered by watsonx.ai
 `docker tag <local_image> us.icr.io/<my_namespace>/<my_repo>`
 `docker push us.icr.io/<my_namespace>/<my_repo>`
 
+`ibmcloud login --apikey <API_KEY>`
+`ibcloud target -g <RESOURCE_GROUP>`
+`ibmcloud cr login --client podman` 
+`podman build -t <image_name> .`
+`podman tag <local_image> us.icr.io/<my_namespace>/<my_repo>`
+`podman push us.icr.io/<my_namespace>/<my_repo>`
+
+`podman build -t contracts-backend .`
+`ibmcloud login --apikey 1XSjOitJCXS1BhdPfA9iNeYVnOEjFZYQrzwmyniFZXUT`
+`ibmcloud target -g itz-wxd-rfbv6e_67bfce9fc`
+`ibmcloud cr login --client podman` 
+`podman tag localhost/contracts-backend us.icr.io/cr-itz-rfbv6e/contracts-backend:latest`
+`podman push us.icr.io/cr-itz-rfbv6e/contracts-backend:latest`
--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import pandas as pd
 import json
 
 from dotenv import load_dotenv
-
+import time
 load_dotenv()
 
 IBM_CLOUD_URL= os.environ["IBM_CLOUD_URL"]
@@ -268,64 +268,130 @@ def streamlit_app():
         # stop_sequence=stop_sequence
         )
     
-    st.write("Upload job posting and cadidate CVs then click generate.")    
-    uploaded_job_file = st.file_uploader("Choose Job Posting PDF files",  type=["pdf"], accept_multiple_files=False)
-    uploaded_cv_files = st.file_uploader("Choose Candidate CV PDF files",  type=["pdf"], accept_multiple_files=True)
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        st.write("Upload job posting and cadidate CVs then click generate.")    
+        uploaded_job_file = st.file_uploader("Choose Job Posting PDF files",  type=["pdf"], accept_multiple_files=False)
+        uploaded_cv_files = st.file_uploader("Choose Candidate CV PDF files",  type=["pdf"], accept_multiple_files=True)
 
-    if uploaded_job_file is not None and uploaded_cv_files is not None:
-        generate_button_disabled = False
-    else:
-        generate_button_disabled = True
-        
-        
-    assessment_report = []
-    if st.button("Generate Assessment", disabled=generate_button_disabled):
-        
-        # Exctract the pdf files
-        extracted_job_file_data = extract_text_from_pdfs(uploaded_job_file)
-        job_posting_extracted_text = next(iter(extracted_job_file_data.values()))
-        extracted_cv_file_data = extract_text_from_pdfs(uploaded_cv_files)
-        
-        # Start generating the assessments
-        st.subheader("Detailed Assessments", divider="gray")
-        for index, (key, value) in enumerate(extracted_cv_file_data.items()):
-            with st.spinner(f"Analyzing resume ({index+1}/{len(extracted_cv_file_data)}): {key}...", show_time=True):
-                json_string_output_raw=llm.generate_text(
-                    prompt=generate_json_prompt.format(json_schema=json_schema,candidate_resume_text=value, job_posting_text=job_posting_extracted_text),
-                    )
-                logger.debug(f"json_string_output_raw:")
-                logger.debug(json_string_output_raw)
-                json_string_output_clean = json_string_output_raw[9:-3]
-                logger.debug(f"json_string_output_clean:")
-                logger.debug(json_string_output_clean)
-                jsonify_output = json.loads(json_string_output_clean)
-                logger.debug(f"jsonify_output:")
-                logger.debug(jsonify_output)
-                assessment_report.append(jsonify_output)
-                
-                # st.write(jsonify_output)
-                
-                with st.expander(f"{jsonify_output['name']}"):
-                    st.write(f"Candidate Name: {jsonify_output['name']}")
-                    st.write(f"Suitability: {jsonify_output['suitability']}")
-                    st.write(f"Score: {jsonify_output['score']}")
-                    st.write(f"Recommended: {jsonify_output['recommended']}")
+        if uploaded_job_file is not None and uploaded_cv_files is not None:
+            generate_button_disabled = False
+        else:
+            generate_button_disabled = True
+            
+            
+        assessment_report = []
+        if st.button("Generate Assessment", disabled=generate_button_disabled):
+            
+            # Exctract the pdf files
+            extracted_job_file_data = extract_text_from_pdfs(uploaded_job_file)
+            job_posting_extracted_text = next(iter(extracted_job_file_data.values()))
+            extracted_cv_file_data = extract_text_from_pdfs(uploaded_cv_files)
+            
+            # Start generating the assessments
+            st.subheader("Detailed Assessments", divider="gray")
+            for index, (key, value) in enumerate(extracted_cv_file_data.items()):
+                with st.spinner(f"Analyzing resume ({index+1}/{len(extracted_cv_file_data)}): {key}...", show_time=True):
+                    json_string_output_raw=llm.generate_text(
+                        prompt=generate_json_prompt.format(json_schema=json_schema,candidate_resume_text=value, job_posting_text=job_posting_extracted_text),
+                        )
+                    logger.debug(f"json_string_output_raw:")
+                    logger.debug(json_string_output_raw)
+                    json_string_output_clean = json_string_output_raw[9:-3]
+                    logger.debug(f"json_string_output_clean:")
+                    logger.debug(json_string_output_clean)
+                    jsonify_output = json.loads(json_string_output_clean)
+                    logger.debug(f"jsonify_output:")
+                    logger.debug(jsonify_output)
+                    assessment_report.append(jsonify_output)
                     
-                    st.write("Detailed Assessment")
-                    st.write(jsonify_output["detailed_assessment"])
+                    # st.write(jsonify_output)
+                    
+                    with st.expander(f"{jsonify_output['name']}"):
+                        st.write(f"Candidate Name: {jsonify_output['name']}")
+                        st.write(f"Suitability: {jsonify_output['suitability']}")
+                        st.write(f"Score: {jsonify_output['score']}")
+                        st.write(f"Recommended: {jsonify_output['recommended']}")
+                        
+                        st.write("Detailed Assessment")
+                        st.write(jsonify_output["detailed_assessment"])
 
-        st.subheader("Assessment Summary", divider="gray")
-        df = pd.DataFrame(assessment_report)
-        # print(df)
-        # filter out the detailed assessment in the table summary.
-        df_overview = df[["name", "suitability", "score", "recommended"]]
-        st.dataframe(df_overview, column_config={
-            "Name": st.column_config.Column(label="Candidate Name",width="medium"), 
-            "Suitability": st.column_config.Column(label="Suitability",width="small"),
-            "Score": st.column_config.Column(label="Score",width="small"),
-            "Recommended": st.column_config.Column(label="Recommended",width="small"),
-            # "Detailed Assessment": st.column_config.Column(label="Detailed Assessment",width="large"),
-        })
+            st.subheader("Assessment Summary", divider="gray")
+            df = pd.DataFrame(assessment_report)
+            # print(df)
+            # filter out the detailed assessment in the table summary.
+            df_overview = df[["name", "suitability", "score", "recommended"]]
+            st.dataframe(df_overview, column_config={
+                "Name": st.column_config.Column(label="Candidate Name",width="medium"), 
+                "Suitability": st.column_config.Column(label="Suitability",width="small"),
+                "Score": st.column_config.Column(label="Score",width="small"),
+                "Recommended": st.column_config.Column(label="Recommended",width="small"),
+                # "Detailed Assessment": st.column_config.Column(label="Detailed Assessment",width="large"),
+            })
+    
+    with col2:
+        with st.container():
+            st.subheader("Assistant")
+            # Initialize chat history
+            if "messages" not in st.session_state:
+                st.session_state.messages = []
+
+            # Create a container for the chat history with scrollable behavior
+            chat_history_container = st.container( key="chat-history" )
+
+            # Display chat messages from history on app rerun
+            with st.container():
+                with chat_history_container:
+                    for message in st.session_state.messages:
+                        with st.chat_message(message["role"]):
+                            st.markdown(message["content"])
+
+            # React to user input
+            with st.container(key="stChatInputContainer"):
+                if prompt := st.chat_input("What can I help you with?", key="user-input"):
+                    # Display user message in chat message container
+                    with chat_history_container:
+                        st.chat_message("user").markdown(prompt)
+                    # Add user message to chat history
+                    st.session_state.messages.append({"role": "user", "content": prompt})
+
+                    response = f"Echo: {prompt}"
+                    # Display assistant response in chat message container
+                    with chat_history_container:
+                        time.sleep(1)
+                        with st.chat_message("assistant"):
+                            st.markdown(response)
+                    # Add assistant response to chat history
+                    st.session_state.messages.append({"role": "assistant", "content": response})
+
+            # Add CSS to ensure chat input stays at the bottom and chat history scrolls
+            st.markdown(
+                """
+                <style>
+                .st-key-user-input {
+                    position: fixed;
+                    bottom: 10px;
+                    width: 100%;
+                }
+                .st-key-parent-chat-history > div {
+                    width: 500px;
+                }
+                .st-key-chat-history {
+                    position: fixed;
+                    bottom: 110;
+                    width: auto;
+                    height: 500px;
+                    overflow: auto;
+                    border: 1px solid white
+                    border-radius: 10px;
+                    
+                    
+                }
+                </style>
+                """,
+                unsafe_allow_html=True,
+            )
         
 if __name__ == "__main__":
     streamlit_app()
